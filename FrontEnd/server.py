32c3825d6c45a9564d0319b0e103fc4390768119
Zihao Fan <fanzihao.thu@gmail.com>
2016-07-28 15:57:38 -0700

    before merge search engine

--- FrontEnd/server.py
+++ FrontEnd/server.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
+
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 app = Flask(__name__)
 app_root = app.root_path
+app.config['DEBUG'] = True
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/query/<query>')
+def search(query):
+
+    pass
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=1339)
