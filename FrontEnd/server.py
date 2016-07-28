2eeda83104feeae813894eb214af09a22df2fb05
Zihao Fan <fanzihao.thu@gmail.com>
2016-07-28 16:42:40 -0700

    add thulac, before search engine

--- FrontEnd/server.py
+++ FrontEnd/server.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
-
+import sys
 import os
+
+current_path = sys.path[0]
+
+search_engine_path = os.path.join(current_path, "..", "SearchEngine", "script")
+lac_path = os.path.join(current_path, "..", "LAC")
+
+sys.path.append(lac_path)
+sys.path.append(search_engine_path)
+
+import thulac
+import SearchEngine
+
+thu = thulac.thulac("-seg_only")
+
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+
 app = Flask(__name__)
 app_root = app.root_path
 app.config['DEBUG'] = True
@@ -15,8 +30,9 @@ def index():
 
 @app.route('/query/<query>')
 def search(query):
-
-    pass
+    query = query.encode("utf-8")
+    print thu.cut(query)
+    return '{"result":true,"count":1}'
 
 
 if __name__ == '__main__':
